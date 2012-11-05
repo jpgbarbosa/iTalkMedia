0432f9ae8725cce44fe91510e0b7b253030d2dcb
João Barbosa <j.barbosa333@gmail.com>
2012-11-05 22:39:18 +0000

    bootstrap init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ITalkMedia::Application.config.secret_token = '5bc4276a56a7ccc7a5be5e6488974bb1ce4e80771774b8e8fe240a59771d260a4340c11f071f5cb67a7ef65e092557ffd23d0be920715af7b115d9f96257b87c'
