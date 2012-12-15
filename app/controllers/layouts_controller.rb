aaee61ed4c2cd05167c66fa88b47d1d4a421e6f0
bernardino <ricardo.bernardino@gmail.com>
2012-12-15 18:05:59 +0000

    get songs not played for a long time and concerts for landing page recommendation

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -1,6 +1,11 @@
 class LayoutsController < ApplicationController
     
   def landing
+    @old_played = Music.get_old_played_songs
+    ap @old_played
+    @concerts = Music.get_upcoming_concerts
+    ap @concerts
+
     render :layout => "landing"
   end
 
